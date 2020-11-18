7fed4efb6a8f021c48adae414ae6cfc5c9903b66
endophyte <74672028+endophyte@users.noreply.github.com>
2020-11-18 19:33:30 +0100

    Add flexible indexing and display of test-data item.

--- tutorials/scikit-learn/basic.py
+++ tutorials/scikit-learn/basic.py
@@ -1,12 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn import datasets
 from sklearn import svm
-iris = datasets.load_iris()
+
+# load the digits dataset
 digits = datasets.load_digits()
 
-### SVM
+# index to test-data element (excluded from training set)
+idx = 0
+
+# display digit
+plt.figure(1, figsize=(3, 3))
+plt.imshow(digits.images[idx], cmap=plt.cm.gray_r, interpolation='nearest')
+print (digits.images[idx])
+
+# define index range
+idx_range = np.r_[0:idx,idx+1:] if idx > 0 else slice(idx+1,len(digits.data))
+
+# classify test-data element via SVM
 clf = svm.SVC(gamma=0.001, C=100.)
-clf.fit(digits.data[1:], digits.target[1:])
-a = clf.predict([digits.data[70]])
+clf.fit(digits.data[idx_range], digits.target[idx_range])
+a = clf.predict([digits.data[idx]])
+print("Result: ", a)
 
-
-print('end')
+plt.show()
